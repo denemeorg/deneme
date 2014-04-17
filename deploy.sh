9e2d2ded798e94e6810ff4e81b2fef48948afb18
Osman Yuksel <yuxel@sonsuzdongu.com>
2014-04-17 15:31:40 +0300

    fo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ workspace_dir="${WORKSPACE}"
 build_base="${WORKSPACE}/../builds/${BUILD_NUMBER}"
 build_dir="${build_base}/build"
 git_files="${build_base}/build/.git"
-build_file="${build_base}/`date +%Y-%m-%d`-build${BUILD_NUMBER}.tar.gz"
+release_file="${build_base}/`date +%Y-%m-%d`-build${BUILD_NUMBER}.tar.gz"
 
 echo -n "Copying files to build directory..."
 cp -r ${workspace_dir} ${build_dir}
@@ -13,5 +13,5 @@ echo "DONE"
 
 
 echo -n "Creating tar.gz file..."
-tar -czvf ${build_file} --directory=${build_dir} .
-echo "DONE"
+tar -czvf ${release_file} --directory=${build_dir} . &>/dev/null
+echo "DONE. File created ${release_file}"
