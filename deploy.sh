7063c6d184816d6342a38f3f18e5a10178844632
Osman Yuksel <yuxel@sonsuzdongu.com>
2014-04-17 14:10:44 +0300

    f

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+workspace_dir="${WORKSPACE}"
+build_base="${env.WORKSPACE}/../builds/${env.BUILD_NUMBER}"
+build_dir="${build_base}/build"
+
+echo "-------------- here comes deploy script ----------"
+echo "workspace_dir: $workspace_dir"
+echo "build_base: $build_base"
+echo "build_dir: $build_dir"
